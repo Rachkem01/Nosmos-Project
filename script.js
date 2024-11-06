0f4a9c9a17a3c68255de570ad6dc9f34a87c1861
Rachael <agunlokokemi01@gmail.com>
2024-11-06 16:20:31 +0100

    uploaded files

---
+++ script.js
@@ -0,0 +1,9 @@
+const ul = document.querySelector('.nav1 ul')
+const bar = document.querySelector('.fa')
+
+console.log(ul)
+console.log(bar)
+bar.addEventListener('click', ()=>{
+    ul.classList.toggle('active')
+    console.log(error)
+})
